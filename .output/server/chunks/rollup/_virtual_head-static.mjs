6c08dadb91c7df4d2c826268b20eb743826e4baa
Emrehrz_ceng <horuzemre3@gmail.com>
2023-05-08 17:47:32 +0300

    last updates

--- .output/server/chunks/rollup/_virtual_head-static.mjs
+++
@@ -1,4 +0,0 @@
-const _virtual__headStatic = {"headTags":"<meta charset=\"utf-8\">\n<title>Kaman Palet - Ahşap ve Plastik Palet Satışı | Sıfır ve İkinci El Seçenekleri | Forklift Kiralama ve Taşımacılık Hizmetleri</title>\n<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n<meta name=\"description\" content=\"Kaman Palet olarak; İstanbulun heryerine sıfır ve ikinci el palet tedariği, taşımacılık ve forklift kiralama hizmeti sunuyoruz.\">","bodyTags":"","bodyTagsOpen":"","htmlAttrs":" lang=\"en\"","bodyAttrs":""};
-
-export { _virtual__headStatic as default };
-//# sourceMappingURL=_virtual_head-static.mjs.map
